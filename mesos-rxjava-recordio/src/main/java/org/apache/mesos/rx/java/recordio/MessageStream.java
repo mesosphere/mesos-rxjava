7a66466f06a1c53c39ff3a8eca3d7f199878221b
Charles Ruhland <charles@mesosphere.io>
2015-11-05 15:37:02 -0800

    Factor out `ConcatenatedInputStream` from `MessageStream`
    This change explictly separates two different but related concerns:
    aggregating incoming `ByteBuf`s for the `RecordIOOperator`, and
    parsing RecordIO messages from the aggregated input.
    
    The `ConcatenatedInputStream` class is a general-purpose class for
    treating a queue of `InputStream`s as a single
    `InputStream`. Additional streams can be added to the queue as they
    become available.
    
    The `MessageStream` class parses RecordIO messages from a single
    `InputStream` provided in the constructor. The `InputStream` is
    assumed to be backed by a source of bytes that can grow over time,
    such that end-of-stream only means that all currently available data
    has been consumed; subsequent attempts to read the stream may
    succeed if new data has been received.
    
    This change should make it easier in the future for `RecordIOOperator`
    to support backpressure, as it allows the rate at which input
    `ByteBuf`s are received to be decoupled from the rate at which parsed
    output messages are sent.

---
+++ mesos-rxjava-recordio/src/main/java/org/apache/mesos/rx/java/recordio/MessageStream.java
@@ -0,0 +1,119 @@
+package org.apache.mesos.rx.java.recordio;
+
+import com.google.common.base.Charsets;
+import io.netty.buffer.ByteBuf;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+final class MessageStream {
+
+    @NotNull
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageStream.class);
+
+    /**
+     * Maximum number of base-10 digits in a uint64
+     */
+    private static final int MESSAGE_SIZE_MAX_LENGTH = 20;
+
+    @NotNull
+    private final InputStream source;
+
+    private int messageSizeBytesRead = 0;
+
+    /**
+     * The message size from the stream is provided as a base-10 String representation of an
+     * unsigned 64 bit integer (uint64). Since there is a possibility (since the spec isn't
+     * formal on this, and the HTTP chunked Transfer-Encoding applied to the data stream can
+     * allow chunks to be any size) this field functions as the bytes that have been read
+     * since the end of the last message. When the next '\n' is encountered in the byte
+     * stream, these bytes are converted to a UTF-8 String. This string representation is
+     * then read as a {@code long} using {@link Long#valueOf(String, int)}.
+     */
+    @NotNull
+    private final byte[] messageSizeBytes = new byte[MESSAGE_SIZE_MAX_LENGTH];
+
+    /**
+     * The number of bytes in the encoding is specified as an unsigned (uint64)
+     * However, since arrays in java are addressed and indexed by int we drop the
+     * precision early so that working with the arrays is easier.
+     * Also, a byte[Integer.MAX_VALUE] is 2gb so I seriously doubt we'll be receiving
+     * a message that large.
+     */
+    private int remainingBytesForMessage = 0;
+
+    /**
+     * The allocated {@code byte[]} for the current message being read from the stream.
+     * It is null until the first message is read, and is reassigned for each new message.
+     */
+    @Nullable
+    private byte[] messageBytes;
+
+    public MessageStream(final @NotNull InputStream source) {
+        this.source = source;
+    }
+
+    /**
+     * Gets the next message from this stream, if possible.
+     *
+     * @return the next complete message from the front of this stream, or {@code null} if not enough data is
+     * available.
+     * @throws IOException if an error occurs when reading data from the appended {@link ByteBuf}s.
+     */
+    @Nullable
+    public byte[] next() throws IOException {
+        if (remainingBytesForMessage == 0) {
+
+            while (true) {
+                int i = source.read();
+                if (i == -1) {
+                    return null;
+                }
+
+                byte b = (byte) i;
+                if (b == (byte) '\n') {
+                    break;
+                }
+
+                try {
+                    messageSizeBytes[messageSizeBytesRead++] = b;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    String error = "Message size field exceeds limit of " + messageSizeBytes.length + " bytes";
+                    throw new IllegalStateException(error, e);
+                }
+            }
+
+            final String messageSizeString = new String(messageSizeBytes, 0, messageSizeBytesRead, Charsets.UTF_8);
+            final long messageSize = Long.valueOf(messageSizeString);
+            messageSizeBytesRead = 0;
+
+            if (messageSize > Integer.MAX_VALUE) {
+                LOGGER.warn("specified message size ({}) is larger than Integer.MAX_VALUE. Value will be truncated to int");
+                remainingBytesForMessage = Integer.MAX_VALUE;
+                // TODO: Possibly make this more robust to account for things larger than 2g
+            } else {
+                remainingBytesForMessage = (int) messageSize;
+            }
+
+            messageBytes = new byte[remainingBytesForMessage];
+        }
+
+        if (messageBytes != null) {
+            final int startIndex = messageBytes.length - remainingBytesForMessage;
+            final int bytesRead = source.read(messageBytes, startIndex, remainingBytesForMessage);
+
+            if (bytesRead == -1) {
+                return null;
+            }
+
+            remainingBytesForMessage -= bytesRead;
+        }
+
+        return (remainingBytesForMessage == 0) ? messageBytes : null;
+    }
+
+}
--- mesos-rxjava-recordio/src/main/java/org/apache/mesos/rx/java/recordio/RecordIOOperator.java
+++ mesos-rxjava-recordio/src/main/java/org/apache/mesos/rx/java/recordio/RecordIOOperator.java
@@ -17,19 +17,12 @@
 package org.apache.mesos.rx.java.recordio;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Charsets;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import rx.Observable.Operator;
 import rx.Subscriber;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.SequenceInputStream;
 
 /**
  * An {@link Operator} that can be applied to a stream of {@link ByteBuf} and produce
@@ -66,7 +59,11 @@ public final class RecordIOOperator implements Operator<byte[], ByteBuf> {
         @NotNull
         final Subscriber<? super byte[]> child;
 
-        private final @NotNull MessageStream messageStream = new MessageStream();
+        @NotNull
+        private final ConcatenatedInputStream pendingInput = new ConcatenatedInputStream();
+
+        @NotNull
+        private final MessageStream messageStream = new MessageStream(pendingInput);
 
         RecordIOSubscriber(@NotNull final Subscriber<? super byte[]> child) {
             super(child);
@@ -91,7 +88,7 @@ public final class RecordIOOperator implements Operator<byte[], ByteBuf> {
         @Override
         public void onNext(final ByteBuf buf) {
             try {
-                messageStream.append(buf);
+                pendingInput.append(new ByteBufInputStream(buf));
 
                 byte[] message;
                 while ((message = messageStream.next()) != null) {
@@ -114,109 +111,5 @@ public final class RecordIOOperator implements Operator<byte[], ByteBuf> {
 
     }
 
-
-    private static final class MessageStream {
-
-        private static final @NotNull Logger LOGGER = LoggerFactory.getLogger(MessageStream.class);
-
-        /** Maximum number of base-10 digits in a uint64 */
-        private static final int MESSAGE_SIZE_MAX_LENGTH = 20;
-
-        private @NotNull InputStream concatenatedInput = new ByteArrayInputStream(new byte[0]);
-
-        private int messageSizeBytesRead = 0;
-
-        /**
-         * The message size from the stream is provided as a base-10 String representation of an
-         * unsigned 64 bit integer (uint64). Since there is a possibility (since the spec isn't
-         * formal on this, and the HTTP chunked Transfer-Encoding applied to the data stream can
-         * allow chunks to be any size) this field functions as the bytes that have been read
-         * since the end of the last message. When the next '\n' is encountered in the byte
-         * stream, these bytes are converted to a UTF-8 String. This string representation is
-         * then read as a {@code long} using {@link Long#valueOf(String, int)}.
-         */
-        private final @NotNull byte[] messageSizeBytes = new byte[MESSAGE_SIZE_MAX_LENGTH];
-
-        /**
-         * The number of bytes in the encoding is specified as an unsigned (uint64)
-         * However, since arrays in java are addressed and indexed by int we drop the
-         * precision early so that working with the arrays is easier.
-         * Also, a byte[Integer.MAX_VALUE] is 2gb so I seriously doubt we'll be receiving
-         * a message that large.
-         */
-        private int remainingBytesForMessage = 0;
-
-        /**
-         * The allocated {@code byte[]} for the current message being read from the stream.
-         * It is null until the first message is read, and is reassigned for each new message.
-         */
-        private byte[] messageBytes;
-
-        /**
-         * Appends the contents of {@code buf} to the end of this stream.
-         *
-         * @param buf The {@link ByteBuf} to append.
-         */
-        public void append(final ByteBuf buf) {
-            concatenatedInput = new SequenceInputStream(concatenatedInput, new ByteBufInputStream(buf));
-        }
-
-        /**
-         * Gets the next message from this stream, if possible.
-         *
-         * @return the next complete message from the front of this stream, or {@code null} if not enough data is
-         *     available.
-         * @throws IOException if an error occurs when reading data from the appended {@link ByteBuf}s.
-         */
-        public byte[] next() throws IOException {
-            if (remainingBytesForMessage == 0) {
-
-                while (true) {
-                    int i = concatenatedInput.read();
-                    if (i == -1) {
-                        return null;
-                    }
-
-                    byte b = (byte) i;
-                    if (b == (byte) '\n') {
-                        break;
-                    }
-
-                    try {
-                        messageSizeBytes[messageSizeBytesRead++] = b;
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        String error = "Message size field exceeds limit of " + messageSizeBytes.length + " bytes";
-                        throw new IllegalStateException(error, e);
-                    }
-                }
-
-                final String messageSizeString = new String(messageSizeBytes, 0, messageSizeBytesRead, Charsets.UTF_8);
-                final long messageSize = Long.valueOf(messageSizeString);
-                messageSizeBytesRead = 0;
-
-                if (messageSize > Integer.MAX_VALUE) {
-                    LOGGER.warn("specified message size ({}) is larger than Integer.MAX_VALUE. Value will be truncated to int");
-                    remainingBytesForMessage = Integer.MAX_VALUE;
-                    // TODO: Possibly make this more robust to account for things larger than 2g
-                } else {
-                    remainingBytesForMessage = (int) messageSize;
-                }
-
-                messageBytes = new byte[remainingBytesForMessage];
-            }
-
-            final int startIndex = messageBytes.length - remainingBytesForMessage;
-            final int bytesRead = concatenatedInput.read(messageBytes, startIndex, remainingBytesForMessage);
-
-            if (bytesRead == -1) {
-                concatenatedInput = new ByteArrayInputStream(new byte[0]);
-                return null;
-            }
-
-            remainingBytesForMessage -= bytesRead;
-            return (remainingBytesForMessage == 0) ? messageBytes : null;
-        }
-
-    }
 
 }
